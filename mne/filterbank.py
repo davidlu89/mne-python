ff4df906385ae22d4af1ec76697615978ad534e5
David <davidlu89@gmail.com>
2017-11-01 21:20:25 -0400

    Added decimation

--- mne/filterbank.py
+++ mne/filterbank.py
@@ -15,86 +15,155 @@ class FilterBank(object):
 
     def __init__(self, data,
                  bw=None, cf=None, foi=None,
-                 order=None, **kwargs):
+                 order=None, sfreq=None, decimate_by=None):
 
-        self._data = data
-        self.nch, self.nsamp = self._data.shape
+        self.decimate_by = decimate_by
 
-        self.sfreq = kwargs['sfreq']
-        self.delta_f_ix = int(self.nsamp // self.sfreq)
+        # Organize frequencies of interests
+        self.freqs = foi
+        self.center_f = cf
+        self.bandwidth = bw
+        self._get_all_frequencies()
 
-        error_freqs = ValueError('foi is not defined. If foi is not known, define cf and bw instead.')
-        if bw is None and cf is None:
-            if foi is None:
-                raise error_freqs
-            else:
-                cf, bw = self._get_center_frequencies(foi)
-
-        elif bw is not None and cf is not None:
-            foi = self._get_frequency_of_interests(cf, bw)
-        else:
-            raise error_freqs
-
-        self.bandwidth = bw if bw is not None else self.bandwidth
-        self.center_f = cf if cf is not None else self.center_f
-        self.freqs = foi if foi is not None else self.freqs
         self.nfreqs = self.freqs.shape[0]
 
-        nsamp_ = int(self.bandwidth * 2 * self.delta_f_ix)
+        # Organize data
+        self._data = self._reshape_data(data)
+        self.nch, self.nsamp = self._data.shape
+
+        self.sfreq = sfreq
+        self.int_phz = int(self.nsamp // self.sfreq) # interval per Hz
+        self._nsamp = int(self.bandwidth * 2 * self.int_phz)
 
         # Create a prototype filter
-        self.order = order if order is not None else self.order
+        self.order = order
+        self.filts = self._create_prototype_filter(self.order, self.bandwidth/2., self.sfreq, self.nsamp)
 
-        _, filts = self.create_filter(self.order, self.bandwidth/2., self.sfreq/2., self.nsamp, shift=True)
+        # Create indices for efficiently filtering the signal
+        self._get_indices_for_frequency_shifts()
 
-        cf0 = self.nsamp//2
-        idx_ = slice(cf0-int(self.delta_f_ix * self.bandwidth), cf0+int(self.delta_f_ix * self.bandwidth))
-        self.filts = np.atleast_2d(filts[idx_])
+    def process(self, data, **kwargs):
+        return self.filter_data(data, self.filts, **kwargs)
 
-        fois_ix = self._get_frequency_of_interests(cf, self.bandwidth * 2) * self.delta_f_ix
+    def filter_data(self, data, filts, axis=-1, mode='hilbert', planner_effort='FFTW_ESTIMATE'):
+        """
+        Perform FFT filtering on the data, given the FFT of predefined filter coefficients.
+        """
+        _data = self._reshape_data(data, axis=axis)
 
-        # Crop out and index of the fft signal, and demodulate them to DC centered
-        tmpwr = np.arange(self.nsamp // 2)
-        idx0 = np.logical_and(tmpwr[:,np.newaxis]>=fois_ix[:,0][np.newaxis,:], tmpwr[:,np.newaxis]<fois_ix[:,-1][np.newaxis,:]).T
-        win_idx0 = np.ones((self.nfreqs, 1)) * np.arange(tmpwr.size)
+        nsampd = self.nsamp // self.decimate_by
+        if mode == 'hilbert':
+            data_f = fft.rfft(_data, axis=axis, planner_effort=planner_effort) / self.decimate_by
 
-        tmp = win_idx0[idx0]
-        diff = int(self.nfreqs * tmp.size // self.nfreqs - tmp.size)
-        if diff < 0:
-            tmp = tmp[:diff]
-        elif diff > 0:
-            tmp = np.hstack([tmp, np.arange(diff)])
+            _X = np.zeros((self.nch, self.nfreqs, nsampd//2), dtype=np.complex64)
+            _X[:, self._idx2, self._idx1] = data_f[:,self._idx1] * filts
 
-        self._idx1 = np.array(tmp.reshape(self.nfreqs, tmp.size // self.nfreqs), dtype=np.int64)
+            return fft.irfft(_X, axis=axis, planner_effort=planner_effort)
 
-        # Shift the cropped out signal back to the modulating frequencies
-        self._idx2 = np.array(np.arange(self.nfreqs)[:, np.newaxis] + np.zeros((1, nsamp_)), dtype=np.int64)
+        else:
+            data_f = fft.fft(_data, axis=axis, planner_effort=planner_effort) / self.decimate_by
 
-    def process(self, data):
-        return self.filter_data(data, self.filts)
+            _X = np.zeros((self.nch, self.nfreqs, nsampd), dtype=np.complex64)
+            _X[:, self._idx2, self._idx1] = data_f[:,self._idx1] * filts
 
-    def filter_data(self, data, filts):
-
-        data_f = np.atleast_2d(fft.rfft(data, planner_effort='FFTW_ESTIMATE'))
-
-        _X = np.zeros((1, self.nfreqs, data_f.size), dtype=np.complex64)
-        _X[:, self._idx2, self._idx1] = data_f[:,self._idx1] * filts
-
-        return fft.irfft(_X, axis=-1, planner_effort='FFTW_ESTIMATE')
+            return fft.ifft(_X * 2, axis=axis, planner_effort=planner_effort)
 
     @staticmethod
-    def _get_center_frequencies(fois):
+    def _reshape_data(data, axis=-1):
+
+        if data.ndim > 2 or axis > 1:
+            raise ("The data should only be in 2 dimensional. \
+                    The support for 3 dimensional have not been implemented yet.")
+        if data.ndim == 1:
+            data = np.atleast_2d(data)
+
+        if axis != 0:
+            return data
+        else:
+            return data.T
+
+    def _create_prototype_filter(self, order, f_cut, fs, N):
+        """
+        Create the prototype filter, which is the only filter require for windowing in the frequency
+        domain of the signal. This filter is a lowpass filter.
+        """
+        cf0 = N // 2
+        idx_ = slice(cf0 - int(self.int_phz * f_cut * 2), cf0 + int(self.int_phz * f_cut * 2))
+        _, filts = self.create_filter(order, f_cut, fs/2., N, shift=True)
+
+        return np.atleast_2d(filts[idx_])
+
+    def _get_indices_for_frequency_shifts(self):
+        """
+        Get the indices for properly shifting the fft of signal to DC, and the indices for shifting
+        the fft of signal back to the correct frequency indices for ifft.
+        """
+        # Crop out and index of the fft signal, and demodulate them to DC centered
+        # The frequency indices for cropping the fft of the signal, which is twice the bandwidth
+        fois_ix = self.get_frequency_of_interests(self.center_f, self.bandwidth * 2) * self.int_phz
+
+        w_ix = np.arange(self.nsamp)
+        idx0 = np.logical_and(w_ix[:,np.newaxis] >= fois_ix[:,0][np.newaxis,:], \
+                              w_ix[:,np.newaxis] < fois_ix[:,-1][np.newaxis,:]).T
+        win_idx0 = (np.ones((self.nfreqs, 1)) * np.arange(w_ix.size))[idx0]
+
+        diff = int(self.nfreqs * win_idx0.size // self.nfreqs - win_idx0.size)
+        if diff < 0:
+            win_idx0 = win_idx0[:diff]
+        elif diff > 0:
+            win_idx0 = np.hstack([win_idx0, np.arange(diff)])
+
+        self._idx1 = np.array(win_idx0.reshape(self.nfreqs, win_idx0.size // self.nfreqs), dtype=np.int64)
+
+        # Shift the cropped out signal back to the modulating frequencies
+        self._idx2 = np.array(np.arange(self.nfreqs)[:, np.newaxis] + np.zeros((1, self._nsamp)), dtype=np.int64)
+
+    def _get_all_frequencies(self):
+        """
+        Ensures all frequencies, fois, cf, and bw.
+        """
+        if (self.center_f, self.bandwidth, self.freqs) is (None, None, None):
+            raise "Must enter one of the following kwargs: 'cf', 'bw', 'fois."
+
+        if self.freqs is None:
+            self.freqs = get_frequency_of_interests(self.center_f, self.bandwidth)
+
+        if self.center_f is None or self.bandwidth is None:
+            self.center_f, self.bandwidth = self.get_center_frequencies(self.freqs)
+
+    @staticmethod
+    def get_center_frequencies(fois):
+        """
+        Provide an array-like frequencies of interests (foi), return the center frequencies (cf) and the bandwidth (bw).
+        The units must be consistant, either all in Hz or all normalized.
+        """
+        if fois.shape[0] == 2:
+            fois = fois.T
+
         cf = np.atleast_2d(fois.mean(axis=-1)).T
         bw = np.diff(fois, axis=-1)
+
+        if not np.diff(bw):
+            bw = float(bw.mean())
+
         return cf, bw
 
     @staticmethod
-    def _get_frequency_of_interests(cf, bw):
-        bw_ = bw * np.ones((cf.shape[0], 2))
-        bw_[:,0] *= -.5
-        bw_[:,1] *= .5
+    def get_frequency_of_interests(cf, bw):
+        """
+        Provide an array-like center frequencies (cf) and bandwidth(s) (bw), return an array of frequency bands.
+        """
+        if cf.ndim == 1:
+            cf = np.atleast_2d(cf).T
+        else:
+            if cf.shape[1] == cf.size:
+                cf = cf.T
 
-        return cf + bw_
+        bw = bw * np.ones((cf.size, 2))
+        bw[:,0] *= -.5
+        bw[:,1] *= .5
+
+        return cf + bw
 
     @staticmethod
     def create_filter(order, cutoff, nyquist, N, ftype='fir', output='freq', shift=True):
